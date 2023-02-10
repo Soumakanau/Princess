3783e3a45862fed7296c1f55f02346374af1e03d
kanau_19 <kananaru0808@gmail.com>
2023-02-10 22:53:53 +0900

    Playerが動くようになった、Playerにカメラが付いてくるようにした

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+    void Update()
+    {
+        transform.position = _target.position;
+    }
+}
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+/// <summary>
+/// キャラクターの移動を作るクラス
+/// </summary>
 public class Mover : MonoBehaviour
 {
     [SerializeField,Header("向かう場所")] Transform _target;
 
-
-    Ray _lastRay;//Rayはクリックしたら消えるから
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);//mouseのpositionにRayを飛ばす
+            MoveToCursor();
         }
-        Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100,Color.red);//Rayの可視化
-        GetComponent<NavMeshAgent>().destination = _target.position;
+    }
+
+    /// <summary>
+    /// MouseClickでPlayerがその場所に移動する
+    /// </summary>
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastHit;
+        bool hasHit = Physics.Raycast(ray, out raycastHit);//光線がどこに当たったかという情報をraycastHit変数に格納
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = raycastHit.point;
+        }
     }
 }
