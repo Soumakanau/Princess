7744dfefc267babc8651ef3e42850fe03815fe10
kanau_19 <kananaru0808@gmail.com>
2023-02-10 20:38:33 +0900

    Rayの可視化、MousePositionにRayを放つ

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,9 +7,15 @@ public class Mover : MonoBehaviour
 {
     [SerializeField,Header("Œü‚©‚¤êŠ")] Transform _target;
 
-    // Update is called once per frame
+
+    Ray _lastRay;//Ray‚ÍƒNƒŠƒbƒN‚µ‚½‚çÁ‚¦‚é‚©‚ç
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);//mouse‚Ìposition‚ÉRay‚ğ”ò‚Î‚·
+        }
+        Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100,Color.red);//Ray‚Ì‰Â‹‰»
         GetComponent<NavMeshAgent>().destination = _target.position;
     }
 }
